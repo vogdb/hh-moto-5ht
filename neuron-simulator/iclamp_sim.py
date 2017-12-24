0db6cf15168d8c5786d8dd880fabc718cf281593
vogdb <vozhdb@gmail.com>
2017-12-24 17:47:17 +0300

    neuron-simulator implementation

---
+++ neuron-simulator/iclamp_sim.py
@@ -0,0 +1,11 @@
+from neuron import h
+import util
+
+soma = util.create_soma()
+
+iclamp = h.IClamp(soma(0))
+iclamp.delay = 5
+iclamp.dur = 1
+iclamp.amp = 0.1  # nA
+
+util.run_sim(soma)
---
+++ neuron-simulator/util.py
@@ -0,0 +1,34 @@
+from neuron import h
+from neuron import gui  # VERY IMPORTANT to include 'gui'! Despite the fact that it is unused.
+from matplotlib import pyplot
+
+
+def create_soma():
+    soma = h.Section(name='soma')
+    soma.L = soma.diam = 12.6157  # microns
+    h.define_shape()  # Translate into 3D points.
+    soma.Ra = 200  # Axial resistance in Ohm * cm
+    soma.cm = 2  # Membrane capacitance in micro Farads / cm^2
+    soma.insert('motoneuron_5ht')
+    return soma
+
+
+def plot_vm(t_vec, v_vec):
+    pyplot.figure(figsize=(8, 4))
+    pyplot.plot(t_vec, v_vec)
+    pyplot.xlabel('time (ms)')
+    pyplot.ylabel('mV')
+    pyplot.show()
+
+
+def run_sim(soma):
+    v_vec = h.Vector()
+    t_vec = h.Vector()
+    v_vec.record(soma(0)._ref_v)
+    t_vec.record(h._ref_t)
+
+    duration = 25.0
+    h.tstop = duration
+    h.run()
+
+    plot_vm(t_vec, v_vec)
