b50d37e9133fe7dd4758d0671afb84df3434df62
vogdb <vozhdb@gmail.com>
2018-01-05 12:20:35 +0300

    neuron simulation parameters

--- test/neuron-simulator/iclamp_sim.py
+++ test/neuron-simulator/iclamp_sim.py
@@ -4,9 +4,9 @@ import util
 
 soma = util.create_soma()
 
-iclamp = h.IClamp(soma(0))
-iclamp.delay = 5
-iclamp.dur = 1
-iclamp.amp = 0.1  # nA
+iclamp = h.IClamp(soma(0.5))
+iclamp.delay = 0
+iclamp.dur = util.RUN_TIME
+iclamp.amp = 0.5  # nA
 
 util.run_sim(soma)
--- test/neuron-simulator/util.py
+++ test/neuron-simulator/util.py
@@ -2,7 +2,7 @@ from neuron import h
 from neuron import gui  # VERY IMPORTANT to include 'gui'! Despite the fact that it is unused.
 from matplotlib import pyplot
 
-RUN_TIME = 25.0
+RUN_TIME = 150.0
 
 
 def create_soma():
@@ -26,7 +26,7 @@ def plot_vm(t_vec, v_vec):
 def run_sim(soma):
     v_vec = h.Vector()
     t_vec = h.Vector()
-    v_vec.record(soma(0)._ref_v)
+    v_vec.record(soma(0.5)._ref_v)
     t_vec.record(h._ref_t)
 
     duration = RUN_TIME
