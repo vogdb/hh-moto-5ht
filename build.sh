d83e24cf7814a2512f57457e3843553bc6627338
vogdb <vozhdb@gmail.com>
2018-01-15 22:15:45 +0300

    improve build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
+#!/usr/bin/env bash
+
 cd build
 find . \! -name '.gitkeep' -delete
 cd ../
 
-NESTML_INSTALL_DIR="${NESTML_INSTALL_DIR:-../../3rdparty/nestml/target}"
-java -jar "$NESTML_INSTALL_DIR"/nestml.jar research_team_models/ --target build/
+NESTML_INSTALL_DIR="${NESTML_INSTALL_DIR:-$HOME/workspace/3rdparty/nestml/target}"
+if [ ! -f $NESTML_INSTALL_DIR/nestml.jar ]; then
+    echo "Please set a correct NESTML_INSTALL_DIR" 1>&2
+    exit 1
+fi
+java -jar $NESTML_INSTALL_DIR/nestml.jar research_team_models/ --target build/
 
 cd build
 cmake -Dwith-nest=$NEST_INSTALL_DIR/bin/nest-config .
